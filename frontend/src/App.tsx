48ccb8949b1098dca43c2a2e601547ef60c9b8bc
jerrydngzh <jerrydeng67@gmail.com>
2024-03-17 01:35:11 -0700

    setup react+vite frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Base of React App</h1>
+      <p>Add a Router here for pages @ react-router-dom</p>
+    </>
+  )
+}
+
+export default App
