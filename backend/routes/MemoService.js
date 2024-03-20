6749822dc88eeebcfd58e557c353c3358baf3791
Johnny <52947117+Toastjpg@users.noreply.github.com>
2024-03-20 14:54:40 -0700

    clean up error handling and remove rendering of views

--- backend/routes/MemoService.js
+++ backend/routes/MemoService.js
@@ -1,150 +1,167 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const Memo = require('../models/MemoSchema');
-const User = require('../models/UserSchema');
+const Memo = require("../models/MemoSchema");
+const User = require("../models/UserSchema");
 
 // NOTE: dev only
 // TODO: secure with Admin role
-router.get('/all', async (req, res) => {
-    const pass = req.query.pass;
+router.get("/all", async (req, res) => {
+  const pass = req.query.pass;
 
-    if (pass !== proess.env.DEV_PASS) {
-        res.status(401).json({message:'Unauthorized'});
-        return;
-    }
+  if (pass !== proess.env.DEV_PASS) {
+    const err = new Error("Unauthorized");
+    err.status = 401;
+    throw err;
+  }
 
-    try {
-        const memos = await Memo.find({});
-        console.log(memos);
-        res.status(200).send(memos);
-    }
-    catch (err) {
-        next(err);
-    }
+  try {
+    const memos = await Memo.find({});
+    console.log(memos);
+    res.status(200).send(memos);
+  } catch (err) {
+    next(err);
+  }
 });
-
 
 // TODO: update to use JWT for auth for user instead of passing a user_id
 // get for a user's memos
-router.get('/:user_id', async (req, res) => {
-    // TODO: validate user_id is a valid user
-    const user_id = req.params.user_id;
+router.get("/:user_id", async (req, res) => {
+  // TODO: validate user_id is a valid user
+  const user_id = req.params.user_id;
 
-    try {
-        // returns array of objects
-        const memos = await Memo.find({ user_id: user_id });
+  try {
+    // returns array of objects
+    const memos = await Memo.find({ user_id: user_id });
 
-        if (memos.length === 0) {
-            return res.status(404).json({message:'No memos found'});
-        }
-
-        console.log(memos);
-        res.status(200).send(memos);
-    } catch (err) {
-        next(err);
+    if (memos.length === 0) {
+      const err = new Error("No memos found");
+      err.status = 404;
+      throw err;
     }
+
+    console.log(memos);
+    res.status(200).send(memos);
+  } catch (err) {
+    next(err);
+  }
 });
 
 // TODO: add in authentication for create
-router.get('/:user_id/:id', async (req, res) => {
-    // TODO: validate for user_id & memo_id pair
-    const memoID = req.params.id;
-    const userID = req.params.user_id;
+router.get("/:user_id/:id", async (req, res) => {
+  // TODO: validate for user_id & memo_id pair
+  const memoID = req.params.id;
+  const userID = req.params.user_id;
 
-    try {
-        const memo = await Memo.findById({ _id: memoID, user_id: userID });
+  try {
+    const memo = await Memo.findById({ _id: memoID, user_id: userID });
 
-        if (!memo) {
-            return res.status(404).json({message:'Memo not found'});
-        }
-
-        res.status(200).send(memo);
-    } catch (err) {
-        next(err);
+    if (!memo) {
+      const err = new Error("No memos found");
+      err.status = 404;
+      throw err;
     }
+
+    res.status(200).send(memo);
+  } catch (err) {
+    next(err);
+  }
 });
 
 // TODO: validation of input
 // TODO: add in authentication for create
-router.post('/:user_id', async (req, res) => {
-    const user_id = req.params.user_id;
-    const memo = {
-        name: req.body.name,
-        date: req.body.date,
-        location: {
-            name: req.body.location.name,
-            coordinates: [req.body.location.coordinates[0], req.body.location.coordinates[1]]
-        },
-        description: req.body.description,
-        user_id: user_id,
-    };
+router.post("/:user_id", async (req, res) => {
+  const user_id = req.params.user_id;
+  const memo = {
+    name: req.body.name,
+    date: req.body.date,
+    location: {
+      name: req.body.location.name,
+      coordinates: [
+        req.body.location.coordinates[0],
+        req.body.location.coordinates[1],
+      ],
+    },
+    description: req.body.description,
+    user_id: user_id,
+  };
 
-    try {
-        const result = await Memo.create(memo);
+  try {
+    const result = await Memo.create(memo);
 
-        if (!result) {
-            console.log(result)
-            res.status(400).json({message:"Document not created"});
-            return;
-        }
-
-        console.log({message:"Created memo", result: result})
-        res.status(201).send(memo);
-    } catch (err) {
-        next(err);
+    if (!result) {
+      console.log(result);
+      const err = new Error("Document not created");
+      err.status = 400;
+      throw err;
     }
+
+    console.log({ message: "Created memo", result: result });
+    res.status(201).send(memo);
+  } catch (err) {
+    next(err);
+  }
 });
 
 // TODO: add in authentication for update
-router.put('/:user_id/:id', async (req, res) => {
-    const memo_id = req.params.id;
-    const userID = req.params.user_id;
+router.put("/:user_id/:id", async (req, res) => {
+  const memo_id = req.params.id;
+  const userID = req.params.user_id;
 
-    // TODO: check for fields to update instead of updating all fields
-    const memoToUpdate = {
-        name: req.body.name,
-        date: req.body.date,
-        location: {
-            name: req.body.location.name,
-            coordinates: [req.body.location.coordinates[0], req.body.location.coordinates[1]]
-        },
-        description: req.body.description
-    };
+  // TODO: check for fields to update instead of updating all fields
+  const memoToUpdate = {
+    name: req.body.name,
+    date: req.body.date,
+    location: {
+      name: req.body.location.name,
+      coordinates: [
+        req.body.location.coordinates[0],
+        req.body.location.coordinates[1],
+      ],
+    },
+    description: req.body.description,
+  };
 
-    try {
-        const memo = await Memo.findOneAndUpdate(
-            { _id: memo_id, user_id: userID },
-            memoToUpdate,
-            { new: true })
+  try {
+    const memo = await Memo.findOneAndUpdate(
+      { _id: memo_id, user_id: userID },
+      memoToUpdate,
+      { new: true }
+    );
 
-        if (!memo) {
-            return res.status(404).json({message: 'Memo not found'});
-        }
-        console.log({message:"Updated memo", result: memo});
-        res.status(200).send(memo);
-    } catch (err) {
-        next(err);
+    if (!memo) {
+      const err = new Error("Memo not found");
+      err.status = 404;
+      throw err;
     }
+    console.log({ message: "Updated memo", result: memo });
+    res.status(200).send(memo);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.delete('/:user_id/:id', async (req, res) => {
-    // TODO: validate user_id & memo_id pair
-    const memo_id = req.params.id;
-    const user_id = req.params.user_id;
+router.delete("/:user_id/:id", async (req, res) => {
+  // TODO: validate user_id & memo_id pair
+  const memo_id = req.params.id;
+  const user_id = req.params.user_id;
 
-    try {
-        const memo = await Memo.findOneAndDelete({ _id: memo_id, user_id: user_id});
+  try {
+    const memo = await Memo.findOneAndDelete({
+      _id: memo_id,
+      user_id: user_id,
+    });
 
-        if (!memo) {
-            return res.status(404).json('Memo not found');
-        }
-
-        console.log("Deleted memo: " + memo); 
-        res.status(200).json({message: 'Deleted memo'});
-    } catch (err) {
-        next(err);
+    if (!memo) {
+      const err = new Error("Memo not found");
+      err.status = 404;
+      throw err;
     }
-});
 
+    console.log("Deleted memo: " + memo);
+    res.status(200).json({ message: "Deleted memo" });
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,17 @@
-var createError = require("http-errors");
 var express = require("express");
-var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
-const PORT = process.env.PORT || 3000;
-const DB_NAME = process.env.DB_NAME || 'test';
+
+const PORT = process.env.PORT || 8080;
+const DB_NAME = process.env.DB_NAME || "test";
 
 var indexRouter = require("./routes/index");
 var MemoServiceRouter = require("./routes/MemoService");
 var UserServiceRouter = require("./routes/UserService");
 var app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
 
 // ============== Setup Middleware ==============
 app.use(logger("dev"));
@@ -25,49 +20,46 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // ============== Setup Routes ==============
-app.use('/', indexRouter);
-app.use('/api/memos', MemoServiceRouter);
-app.use('/api/users', UserServiceRouter);
+app.use("/", indexRouter);
+app.use("/api/memos", MemoServiceRouter);
+app.use("/api/users", UserServiceRouter);
 
 // ========= Error Handling Middleware =========
-// app.use(express.static(path.join(__dirname, 'public')));
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  console.log(err.stack);
 
-  // render the error page
+  // Set status of response
   res.status(err.status || 500);
-  res.render("error");
+
+  // Send response
+  res.json({
+    error: {
+      message: err.message || "Internal Server Error",
+    },
+  });
 });
 
-// Connect to DB 
-mongoose.set('strictQuery', false);
+// Connect to DB
+mongoose.set("strictQuery", false);
 
 // NOTE: for local MongoDB
-mongoose.connect(`mongodb://localhost:27017/${DB_NAME}`)
-    .then(() => {
-      console.log('Connected to MongoDB...');
-      app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-      })
-    })
-    .catch(err => console.error('Could not connect to MongoDB...', err));
+// mongoose.connect(`mongodb://localhost:27017/${DB_NAME}`)
+//     .then(() => {
+//       console.log('Connected to MongoDB...');
+//       app.listen(PORT, () => {
+//         console.log(`Server is running on port ${PORT}`);
+//       })
+//     })
+//     .catch(err => console.error('Could not connect to MongoDB...', err));
 
-// NOTE: for MongoDB Atlas    
-// mongoose
-//   .connect(
-//     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@372-proj.3a6gakd.mongodb.net/?retryWrites=true&w=majority&appName=372-proj`
-//   )
-//   .then(() => {
-//     app.listen(PORT, () => {
-//       console.log(`Server is running on port ${PORT}`);
-//     });
-//   })
-//   .catch((e) => console.error("Failed to connect to Mongo: ", e));
+// NOTE: for MongoDB Atlas
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@372-proj.3a6gakd.mongodb.net/?retryWrites=true&w=majority&appName=372-proj`
+  )
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((e) => console.error("Failed to connect to Mongo: ", e));
