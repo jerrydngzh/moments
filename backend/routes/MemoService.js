477484ab9dd445fd3952ba12ff22e23de6dcc3d9
jerrydngzh <jerrydeng67@gmail.com>
2024-03-20 00:33:54 -0700

    error handling formatting

--- backend/routes/MemoService.js
+++ backend/routes/MemoService.js
@@ -5,10 +5,11 @@ const User = require('../models/UserSchema');
 
 // NOTE: dev only
 // TODO: secure with Admin role
-router.get('/:pass/all', async (req, res) => {
-    const pass = req.params.pass;
-    if (pass !== 'password') {
-        res.status(401).send('Unauthorized');
+router.get('/all', async (req, res) => {
+    const pass = req.query.pass;
+
+    if (pass !== proess.env.DEV_PASS) {
+        res.status(401).json({message:'Unauthorized'});
         return;
     }
 
@@ -18,8 +19,7 @@ router.get('/:pass/all', async (req, res) => {
         res.status(200).send(memos);
     }
     catch (err) {
-        console.log(err);
-        res.status(500).send('Error: ' + err);
+        next(err);
     }
 });
 
@@ -27,40 +27,47 @@ router.get('/:pass/all', async (req, res) => {
 // TODO: update to use JWT for auth for user instead of passing a user_id
 // get for a user's memos
 router.get('/:user_id', async (req, res) => {
-    // const username = req.params.username;
-
     // TODO: validate user_id is a valid user
+    const user_id = req.params.user_id;
 
     try {
         // returns array of objects
         const memos = await Memo.find({ user_id: user_id });
+
+        if (memos.length === 0) {
+            return res.status(404).json({message:'No memos found'});
+        }
+
         console.log(memos);
         res.status(200).send(memos);
     } catch (err) {
-        console.log(err);
-        res.status(500).send('Error: ' + err);
+        next(err);
     }
 });
 
 // TODO: add in authentication for create
-router.get('/:user_id/:id', async (req, res) => {
-    const memo_id = req.params.id;
+router.get('/:user_id/:memo_id', async (req, res) => {
+    // TODO: validate for user_id & memo_id pair
+    const memoID = req.params.memo_id;
     const userID = req.params.user_id;
 
-    // TODO: validate for user_id & memo_id pair
-
     try {
-        const memo = await Memo.findById({ _id: memo_id, user_id: userID });
+        const memo = await Memo.findById({ _id: memoID, user_id: userID });
+
+        if (!memo) {
+            return res.status(404).json({message:'Memo not found'});
+        }
+
         res.status(200).send(memo);
     } catch (err) {
-        console.log(err);
-        res.status(500).send('Error: ' + err);
+        next(err);
     }
 });
 
 // TODO: validation of input
 // TODO: add in authentication for create
-router.post('/', async (req, res) => {
+router.post('/:user_id', async (req, res) => {
+    const user_id = req.params.user_id;
     const memo = {
         name: req.body.name,
         date: req.body.date,
@@ -69,7 +76,7 @@ router.post('/', async (req, res) => {
             coordinates: [req.body.location.coordinates[0], req.body.location.coordinates[1]]
         },
         description: req.body.description,
-        user_id: req.body.user_id,
+        user_id: user_id,
     };
 
     try {
@@ -77,23 +84,23 @@ router.post('/', async (req, res) => {
 
         if (!result) {
             console.log(result)
-            res.status(400).send('Document not created');
+            res.status(400).json({message:"Document not created"});
             return;
         }
 
-        console.log("Created memo: " + result)
+        console.log({message:"Created memo", result: result})
         res.status(201).send(memo);
     } catch (err) {
-        console.log(err);
-        res.status(500).send('Error: ' + err);
+        next(err);
     }
 });
 
-// TODO: add in validation for update 
 // TODO: add in authentication for update
-router.put('/:id', async (req, res) => {
-    const memo_id = req.params.id;
-    const userID = req.body.user_id;
+router.put('/:user_id/:memo_id', async (req, res) => {
+    const memo_id = req.params.memo_id;
+    const userID = req.params.user_id;
+
+    // TODO: check for fields to update instead of updating all fields
     const memoToUpdate = {
         name: req.body.name,
         date: req.body.date,
@@ -111,15 +118,12 @@ router.put('/:id', async (req, res) => {
             { new: true })
 
         if (!memo) {
-            res.status(404).send('Memo not found');
-            return;
+            return res.status(404).json({message: 'Memo not found'});
         }
-
-        console.log("Updated memo: " + memo);
+        console.log({message:"Updated memo", result: memo});
         res.status(200).send(memo);
     } catch (err) {
-        console.log(err);
-        res.status(500).send('Error: ' + err);
+        next(err);
     }
 });
 
@@ -128,24 +132,17 @@ router.delete('/:user_id/:memo_id', async (req, res) => {
     const memo_id = req.params.memo_id;
     const user_id = req.params.user_id;
 
-    if (!memo_id) {
-        res.status(400).send({ error: 'Bad request: memo_id is required' });
-        return;
-    }
-
     try {
         const memo = await Memo.findOneAndDelete({ _id: memo_id, user_id: user_id});
 
         if (!memo) {
-            res.status(404).send('Memo not found');
-            return;
+            return res.status(404).json('Memo not found');
         }
 
         console.log("Deleted memo: " + memo); 
-        res.status(200).send({status:'Deleted memo: ', memo});
+        res.status(200).json({message: 'Deleted memo'});
     } catch (err) {
-        console.log(err);
-        res.status(500).send('Error: ' + err);
+        next(err);
     }
 });
 
