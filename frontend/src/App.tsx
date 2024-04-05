3074a3af74b9582187d7dbfb955b841124309349
jerrydngzh <jerrydeng67@gmail.com>
2024-04-05 13:50:43 -0700

    add in context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,30 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
-
+import CreateAccountPage from './components/CreateAccount/page'; 
 import User from './components/User/page';
 import Profile from './components/Profile/page';
 import Lens from './components/Lens/page';
 import Dashboard from './components/Dashboard/page'; 
 import CreateMemo from './components/CreateMemo/page'; 
 import "./App.css";
-
-import { AuthenticationPage } from './pages/authentication.page';
+import { FirebaseAuthProvider } from './contexts/FirebaseAuth.context';
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<AuthenticationPage />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/lens" element={<Lens />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/createMemo" element={<CreateMemo />} />
-      </Routes>
-    </Router>
+    <FirebaseAuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/user" element={<User />} />
+          <Route path="/createAccount" element={<CreateAccountPage />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/lens" element={<Lens />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/createMemo" element={<CreateMemo />} />
+        </Routes>
+      </Router>
+    </FirebaseAuthProvider>
   );
 }
 
