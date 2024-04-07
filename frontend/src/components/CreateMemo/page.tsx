ad703a09c76b2ef20d3a379535d8309339d7f7de
jerrydngzh <jerrydeng67@gmail.com>
2024-04-07 02:44:54 -0700

    replacing path querying of ids w/ user context + refactoring a bit

--- frontend/src/components/CreateMemo/page.tsx
+++ frontend/src/components/CreateMemo/page.tsx
@@ -1,97 +1,53 @@
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
+
 import MapForm from "./components/LocationMapPicker/Map";
 import SavedLocations from "./components/Locations/SavedLocations";
-import { MemoController } from "../../controllers/memo.controller";
-import { MemoType } from "../../models/memo";
-import { UserController } from "../../controllers/user.controller";
 import Header from "../Header/header";
+
+import { MemoController } from "../../controllers/memo.controller";
+import { UserController } from "../../controllers/user.controller";
+import { MemoType } from "../../models/memo";
+
+import { useFirebaseAuth } from "../../contexts/FirebaseAuth.context";
 
 const CreateMemo = ({}) => {
   const navigate = useNavigate();
-  const [userID, setUserID] = useState<any>(""); // FIXME - get user ID from session state (for itr2)
-  const [name, setName] = useState<any>("");
-  const [description, setMemo] = useState<any>("");
-  const [locationName, setLocationName] = useState<any>("");
+  const [name, setName] = useState("");
+  const [description, setMemo] = useState("");
+  const [locationName, setLocationName] = useState("");
   const [coordinates, setCoordinates] = useState<[number, number]>([
     49.27326489299744, -123.10365200042726,
   ]);
-  const [reloadDropdown, setReloadDropdown] = useState<any>(false);
-  const [selectedLocation, setSelectedLocation] = useState<any>(null);
+  const [reloadDropdown, setReloadDropdown] = useState(false);
+  const [selectedLocation, setSelectedLocation] = useState(null);
   const [userData, setUserData] = useState<any>({});
+  const { currentUser } = useFirebaseAuth();
 
-  const handleLocationSelected = (location: any) => {
-    console.log(location);
-    // Set the selected location in state
-    setSelectedLocation(location);
-  };
+  const fetchLocationData = async () => {
+    const [lat, lon] = coordinates;
 
-  const fetchCategories = async () => {
     try {
-      const searchParams = new URLSearchParams(window.location.search);
-      const idFromQuery = searchParams.get("id") || "";
-      setUserID(idFromQuery);
-      console.log(userID);
+      const response = await fetch(
+        `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}&zoom=18&addressdetails=1`
+      );
 
-      // Fetch memo data from the server
-      const data = await UserController.get_user_data(idFromQuery);
-
-      console.log("Fetched Account:", data);
-
-      setUserData(data);
-    } catch (error) {
-      console.error("Error fetching categories:", error);
-    }
-  };
-
-  useEffect(() => {
-    const searchParams = new URLSearchParams(window.location.search);
-    const idFromQuery = searchParams.get("is") || "";
-    setUserID(idFromQuery);
-    const fetchLocationName = async () => {
-      const [lat, lon] = coordinates;
-
-      try {
-        // NOTE
-        const response = await fetch(
-          `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}&zoom=18&addressdetails=1`
-        );
-
-        if (!response.ok) {
-          const data = await response.json();
-          console.error(
-            "Error fetching location name:",
-            data.error || "Unknown error"
-          );
-          return;
-        }
-
+      if (!response.ok) {
         const data = await response.json();
-
-        if (data.display_name) {
-          setLocationName(data.display_name);
-        } else {
-          console.error(
-            "Error fetching location name: No display name in response"
-          );
-        }
-      } catch (error) {
-        console.error("Error fetching location name:", error);
+        console.error("Error fetching location name:", data.error || "Unknown error");
+        return;
       }
-    };
-    setReloadDropdown(true);
-    fetchLocationName();
-    fetchCategories();
-  }, [coordinates, userID]);
 
-  const handleMapClick = (clickedLocation: [number, number]) => {
-    console.log(clickedLocation);
-    setCoordinates(clickedLocation);
-  };
+      const data = await response.json();
 
-  const handleDropdownReloaded = () => {
-    // Reset reloadDropdown to false after the dropdown has been reloaded
-    setReloadDropdown(false);
+      if (data.display_name) {
+        setLocationName(data.display_name);
+      } else {
+        console.error("Error fetching location name: No display name in response");
+      }
+    } catch (error) {
+      console.error("Error fetching location name:", error);
+    }
   };
 
   const handleSubmit = async (event: any) => {
@@ -109,9 +65,8 @@ const CreateMemo = ({}) => {
     };
 
     try {
-      const res = await MemoController.create_memo(userID, memoToCreate);
-      // console.log(res)
-      const newMemoId = res._id; // TODO
+      const createdMemo = await MemoController.create_memo(currentUser.uid, memoToCreate);
+      const newMemoId = createdMemo._id; // TODO
 
       // Update the memos array in userData with the new memo ID
       let newUserData: any = userData;
@@ -119,10 +74,10 @@ const CreateMemo = ({}) => {
 
       setUserData(newUserData);
 
-      const user = await UserController.update_user(userID, newUserData);
+      const user = await UserController.update_user(currentUser.uid, newUserData);
       console.log(user);
 
-      navigate(`/dashboard?id=${userID}`);
+      navigate("/dashboard");
     } catch (error) {
       console.error("Error creating memo:", error);
     }
@@ -133,25 +88,26 @@ const CreateMemo = ({}) => {
     setMemo("");
   };
 
+  useEffect(() => {
+    setReloadDropdown(true);
+    fetchLocationData();
+  }, [coordinates]);
+
   return (
     <main className="create-memo w-2/3 text-left m-auto mt-10 bg-blue-200 p-10 pr-20 pl-20 rounded-3xl border-2 border-blue-800">
-      <Header id={userID}></Header>
+      <Header />
 
       <form onSubmit={handleSubmit} onReset={handleReset}>
-        <h2 className="text-3xl font-bold mb-6 mt-6 text-blue-800">
-          Creating New Memo
-        </h2>
+        <h2 className="text-3xl font-bold mb-6 mt-6 text-blue-800">Creating New Memo</h2>
+
         <div className="map-container">
           <label htmlFor="location" className="text-xl text-blue-800">
             Location
           </label>
-          <link
-            rel="stylesheet"
-            href="https://unpkg.com/leaflet/dist/leaflet.css"
-          />
+          <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
           <MapForm
             selectedLocation={selectedLocation}
-            onMapClick={handleMapClick}
+            onMapClick={(clickedLocation: [number, number]) => setCoordinates(clickedLocation)}
           />
         </div>
 
@@ -159,56 +115,58 @@ const CreateMemo = ({}) => {
         <div className="input-container">
           <label htmlFor="locationName" className="text-xl text-blue-800">
             Location Name
+            <input
+              type="text"
+              id="locationName"
+              name="locationName"
+              className="location"
+              required
+              value={locationName}
+              onChange={(e) => setLocationName(e.target.value)}
+            />
           </label>
-          <input
-            type="text"
-            id="locationName"
-            name="locationName"
-            className="location"
-            required
-            value={locationName}
-            onChange={(e) => setLocationName(e.target.value)}
-          />
         </div>
 
         {/* Displays Saved Locations */}
         <SavedLocations
           reloadDropdown={reloadDropdown}
-          id={userID}
-          onDropdownReloaded={handleDropdownReloaded}
-          onLocationSelected={handleLocationSelected}
+          id={currentUser.uid}
+          onDropdownReloaded={() => setReloadDropdown(false)}
+          onLocationSelected={(location) => setSelectedLocation(location)}
         />
 
         {/* Title for Memo */}
         <div className="input-container">
           <label htmlFor="name" className="text-xl text-blue-800">
             Title
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="name"
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            className="name"
-            required
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
         </div>
 
         {/* Description for the Memo */}
         <div className="input-container">
           <label htmlFor="memo" className="text-xl text-blue-800">
             Memo
+            <textarea
+              id="memo"
+              name="memo"
+              className="memo"
+              required
+              value={description}
+              onChange={(e) => setMemo(e.target.value)}
+            />
           </label>
-          <textarea
-            id="memo"
-            name="memo"
-            className="memo"
-            required
-            value={description}
-            onChange={(e) => setMemo(e.target.value)}
-          />
         </div>
+
+        {/* Button Nav Bar */}
         <div className="button-container ">
           <input
             className="border-2 border-blue-800 w-full text-xl text-blue-800 h-14 mb-2 mt-8 bg-blue-100 hover:bg-white rounded-xl"
