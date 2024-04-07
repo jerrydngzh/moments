6974ee8a664011e30ff62b696b21637976f4fded
Johnny <52947117+Toastjpg@users.noreply.github.com>
2024-04-06 20:19:42 -0700

    add  toggle edit to profile

--- frontend/src/components/Profile/page.tsx
+++ frontend/src/components/Profile/page.tsx
@@ -19,8 +19,8 @@ export default function Profile() {
     status_message: "",
     profile_picture: "",
   });
-
   const [id, setID] = useState("");
+  const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
     const searchParams = new URLSearchParams(window.location.search);
@@ -66,39 +66,59 @@ export default function Profile() {
       <Header id={id}></Header>
 
       <h2 className="text-blue-800 text-3xl mb-4">User Profile</h2>
+
+      {/* TODO: Extract user data section to component */}
       <p className="italic text-blue-800">
         Name: {userData.first_name} {userData.last_name}
       </p>
       <p className="italic text-blue-800">Username: {userData.username}</p>
       <p className="italic text-blue-800">Email: {userData.email}</p>
 
-      <input
-        type="text"
-        name="bio"
-        value={profileData.bio}
-        onChange={handleChange}
-        className="mb-2"
-      />
-      <input
-        type="text"
-        name="status_message"
-        value={profileData.status_message}
-        onChange={handleChange}
-        className="mb-2"
-      />
-      <input
-        type="text"
-        name="profile_picture"
-        value={profileData.profile_picture}
-        onChange={handleChange}
-        className="mb-2"
-      />
-      <button
-        onClick={handleUpdateProfile}
-        className="button-link text-blue-800 bg-blue-100 border-blue-800 border-2 p-2 text-center rounded-lg"
-      >
-        Update Profile
-      </button>
+      {/* Extract profile data to component... */}
+
+      {editMode ? (
+        <>
+          <input
+            type="text"
+            name="bio"
+            value={profileData.bio}
+            onChange={handleChange}
+            className="mb-2"
+          />
+          <input
+            type="text"
+            name="status_message"
+            value={profileData.status_message}
+            onChange={handleChange}
+            className="mb-2"
+          />
+          <button
+            onClick={() => setEditMode(false)}
+            className="button-link text-blue-800 bg-blue-100 border-blue-800 border-2 p-2 text-center rounded-lg"
+          >
+            Cancel
+          </button>
+          <button
+            onClick={handleUpdateProfile}
+            className="button-link text-blue-800 bg-blue-100 border-blue-800 border-2 p-2 text-center rounded-lg"
+          >
+            Update Profile
+          </button>
+        </>
+      ) : (
+        <>
+          <p className="italic text-blue-800">
+            Current Status: {profileData.status_message}
+          </p>
+          <p className="italic text-blue-800">User Bio: {profileData.bio}</p>
+          <button
+            onClick={() => setEditMode(true)}
+            className="button-link text-blue-800 bg-blue-100 border-blue-800 border-2 p-2 text-center rounded-lg"
+          >
+            Edit Profile
+          </button>
+        </>
+      )}
     </main>
   );
 }
