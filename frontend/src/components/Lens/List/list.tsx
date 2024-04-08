3cc9cba9c432afea868b4ec1da13c956b9ef50cd
Vince <112585869+vinn03@users.noreply.github.com>
2024-04-07 19:04:11 -0700

    added sort functions: date-asc, date-desc, title-asc, tile-desc to the list view

--- frontend/src/components/Lens/List/list.tsx
+++ frontend/src/components/Lens/List/list.tsx
@@ -17,14 +17,50 @@ interface MapProps {
 
 const List: React.FC<MapProps> = ({ locations }) => {
   const [selectedMemo, setSelectedMemo] = useState(null);
+  const [sortedMemos, setSortedMemos] = useState([]);
+  const [sortStateIndex, setSortStateIndex] = useState(3); // state variable to keep track of the current index
+
+  const sortStates = ['title-asc', 'title-desc', 'date-asc', 'date-desc']; // array of sort states
+  const sortStateDescriptions = {
+    'title-asc': 'Title (Ascending)',
+    'title-desc': 'Title (Descending)',
+    'date-asc': 'Date (Ascending)',
+    'date-desc': 'Date (Descending)',
+  };
   
   const handleListItemClick = (memo) => {
     selectedMemo === memo ? setSelectedMemo(null) : setSelectedMemo(memo);
   };
   
+  const handleSortButtonClick = () => {
+    const newSortStateIndex = (sortStateIndex + 1) % sortStates.length; // calculate the new sort state index
+    const sortState = sortStates[newSortStateIndex]; // use the new sort state index to get the sort state
+    const [sortType, sortOrder] = sortState.split('-');
+  
+    const sortedLocations = [...locations].sort((a, b) => {
+      if (sortType === 'title') {
+        const aTitle = a.memo[0]?.title || '';
+        const bTitle = b.memo[0]?.title || '';
+        return sortOrder === 'asc' ? aTitle.localeCompare(bTitle) : bTitle.localeCompare(aTitle);
+      } else if (sortType === 'date') {
+        const aDate = a.memo[0]?.date ? Date.parse(a.memo[0]?.date) : 0;
+        const bDate = b.memo[0]?.date ? Date.parse(b.memo[0]?.date) : 0;
+        return sortOrder === 'asc' ? aDate - bDate : bDate - aDate;
+      }
+    });
+  
+    setSortedMemos(sortedLocations);
+    setSortStateIndex(newSortStateIndex); // update the state with the new sort state index
+  };
+  
+  const memosToDisplay = sortedMemos.length > 0 ? sortedMemos : locations;
+
   return (
     <>
-      {locations.map((location, locIndex) => (
+      <div className='text-blue-800 bg-blue-100 hover:bg-white w-1/2 mb-2 p-2 text-center rounded-lg'>
+        <button onClick={handleSortButtonClick}>Sort Memos: {sortStateDescriptions[sortStates[sortStateIndex]]}</button>      
+      </div>
+      {memosToDisplay.map((location, locIndex) => (
         <ul key={locIndex}>
           {location.memo.map((memo, memoIndex) => (
             <>
