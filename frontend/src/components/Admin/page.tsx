2d881644f12217ae6b710ae7e8ccb041538148bf
Johnny <52947117+Toastjpg@users.noreply.github.com>
2024-04-08 22:57:07 -0700

    add header to page

--- frontend/src/components/Admin/page.tsx
+++ frontend/src/components/Admin/page.tsx
@@ -7,14 +7,16 @@ export default function AdminPage() {
     <div className="w-2/3 text-left m-auto mt-10 bg-blue-200 p-10 pr-20 pl-20 rounded-3xl border-2 border-blue-800">
       <Header />
 
-      <h1 className="font-bold mt-6 text-blue-800">All Users</h1>
+      <h1 className="font-bold mt-6 text-blue-800 ">Admin Control Panel</h1>
+
+      <h1 className="font-bold mt-6 text-blue-800 text-3xl">All Users</h1>
       <div className="relative flex">
         <div className="flex-grow border border-blue-800 mt-3 mb-3"></div>
       </div>
 
       <AdminUserList />
 
-      <h1 className="font-bold text-blue-800">All Memos</h1>
+      <h1 className="font-bold text-blue-800 text-3xl">All Memos</h1>
       <div className="relative flex">
         <div className="flex-grow border border-blue-800 mt-3 mb-3"></div>
       </div>
