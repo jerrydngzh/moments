e18115803310e8d6097568d163356538a50354c2
Arthur Lee <lspa732@gmail.com>
2024-04-09 15:48:45 -0700

    added Mapdisplay in Dashboard popup

--- frontend/src/components/Dashboard/components/Popup/Popup.tsx
+++ frontend/src/components/Dashboard/components/Popup/Popup.tsx
@@ -3,72 +3,93 @@ import { MemoController } from '../../../../controllers/memo.controller';
 import { MemoType } from '../../../../models/memo';
 import { useFirebaseAuth } from "../../../../contexts/FirebaseAuth.context";
 import MemoForm from '../../../CreateMemo/components/MemoForm/memoForm';
+import Map from '../../../Lens/Map/map';
 import "./Popup.css"
+
 const Popup = (props:{
-  onClick: React.MouseEventHandler<HTMLDivElement>, 
-  selectedMemo:MemoType, 
-  handleClose:(event:React.MouseEvent<HTMLButtonElement, MouseEvent>) => void, 
-  Key:boolean }) => {
-  const [reloadKey, setReloadKey] = useState(props.Key);
-  const [editing, setEditing] = useState(false);
-  const {currentUser} = useFirebaseAuth();
+    onClick: React.MouseEventHandler<HTMLDivElement>, 
+    selectedMemo:MemoType, 
+    handleClose:(event:React.MouseEvent<HTMLButtonElement, MouseEvent>) => void, 
+    Key:boolean }) => {
+    const [reloadKey, setReloadKey] = useState(props.Key);
+    const [editing, setEditing] = useState(false);
+    const {currentUser} = useFirebaseAuth();
 
-  useEffect(() => {
-    if (reloadKey) {
-      setReloadKey(false);
-    }
-  }, [reloadKey, props.selectedMemo]);
+    const location = ()=> {
+      const coordinates = props.selectedMemo.location.coordinates;
+      const memo_location_obj = {
+        title: props.selectedMemo.name,
+        description: props.selectedMemo.description,
+        date: props.selectedMemo.date,
+        location: props.selectedMemo.location.name,
+      };
+      return {
+        coordinates:coordinates,
+        memo:[memo_location_obj]
+      }
 
-  const handleEditClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    event.stopPropagation();
-    setEditing(true);
-};
-
-  const handleEditSubmit = async(name:string, description:string, locationName:string, coordinates:[number,number] ) => {
-    // For now, let's just log the new title and memo
-    props.selectedMemo.name = name;
-    props.selectedMemo.description = description;
-    props.selectedMemo.location = {
-      name: locationName,
-      coordinates: coordinates,
-    }
-    const updatedMemo = {
-      id: props.selectedMemo.id,
-      description: props.selectedMemo.description,
-      date: new Date().toLocaleString(),
-      name: props.selectedMemo.name,
-      location: props.selectedMemo.location
     };
+    useEffect(() => {
+      if (reloadKey) {
+        setReloadKey(false);
+      }
+    }, [reloadKey, props.selectedMemo]);
 
-    try{
-      await MemoController.update_memo(currentUser.uid, updatedMemo);
-    } catch (error) {
-      console.error('Error updating memo:', error);
-    }
-    setEditing(false);
+    const handleEditClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      event.stopPropagation();
+      setEditing(true);
   };
 
-  return (
-    <div className="popup" onClick={props.onClick}>
-      <h2>Memo Details</h2>
-      {editing ? (
-        <>
-        <div className='MemoForm'>
-          <MemoForm onSubmit={handleEditSubmit} default_name={props.selectedMemo.name} default_description={props.selectedMemo.description}/>
-        </div>
-        </>
-      ) : (
-      <>
-        <p><strong>Title:</strong> {props.selectedMemo.name}</p>
-        <p><strong>Location:</strong> {props.selectedMemo.location.name}</p>
-        <p><strong>Date:</strong> {props.selectedMemo.date}</p>
-        <p><strong>Memo:</strong> {props.selectedMemo.description}</p>
-        <button onClick={(event) => handleEditClick(event)}>Edit</button>
-        </>
-      )}
-      <button onClick={props.handleClose}>Close</button>
-    </div>
-  );
-};
+    const handleEditSubmit = async(name:string, description:string, locationName:string, coordinates:[number,number] ) => {
+      // For now, let's just log the new title and memo
+      props.selectedMemo.name = name;
+      props.selectedMemo.description = description;
+      props.selectedMemo.location = {
+        name: locationName,
+        coordinates: coordinates,
+      }
+      const updatedMemo = {
+        id: props.selectedMemo.id,
+        description: props.selectedMemo.description,
+        date: new Date().toLocaleString(),
+        name: props.selectedMemo.name,
+        location: props.selectedMemo.location
+      };
 
-export default Popup;
+      try{
+        await MemoController.update_memo(currentUser.uid, updatedMemo);
+      } catch (error) {
+        console.error('Error updating memo:', error);
+      }
+      setEditing(false);
+    };
+
+    return (
+      <div className="popup" onClick={props.onClick}>
+        <h2>Memo Details</h2>
+        {editing ? (
+          <>
+          <div className='MemoForm'>
+            <MemoForm onSubmit={handleEditSubmit} default_name={props.selectedMemo.name} default_description={props.selectedMemo.description}/>
+          </div>
+          </>
+        ) : (
+        <>
+        <div className='MapDisplay w-3/4 h-auto m-auto'>
+            <Map 
+            locations={[location()]} view={''}></Map>
+          </div>
+          <p><strong>Title:</strong> {props.selectedMemo.name}</p>
+          <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+          <p><strong>Location:</strong> {props.selectedMemo.location.name}</p>
+          <p><strong>Date:</strong> {props.selectedMemo.date}</p>
+          <p><strong>Memo:</strong> {props.selectedMemo.description}</p>
+          <button onClick={(event) => handleEditClick(event)}>Edit</button>
+          </>
+        )}
+        <button onClick={props.handleClose}>Close</button>
+      </div>
+    );
+  };
+
+  export default Popup;
