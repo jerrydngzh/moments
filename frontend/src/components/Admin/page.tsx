fe824c97a690493553c1001102551895eede465d
Johnny <52947117+Toastjpg@users.noreply.github.com>
2024-04-08 21:45:50 -0700

    add admin route guard, empty page, and access point

---
+++ frontend/src/components/Admin/page.tsx
@@ -0,0 +1,10 @@
+import Header from "../Header/header";
+
+export default function AdminPage() {
+  return (
+    <div className="w-2/3 text-left m-auto mt-10 bg-blue-200 p-10 pr-20 pl-20 rounded-3xl border-2 border-blue-800">
+      <Header></Header>
+      This is the admin page lol
+    </div>
+  );
+}
