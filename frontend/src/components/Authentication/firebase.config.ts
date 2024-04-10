c29281502a7f7d28e6a39d98388e4dbc2cbb915b
Arthur Lee <lspa732@gmail.com>
2024-04-09 20:32:11 -0700

    added media upload to local storage and display, need more styling and testing

--- frontend/src/components/Authentication/firebase.config.ts
+++ frontend/src/components/Authentication/firebase.config.ts
@@ -4,12 +4,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APP_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyAI-dkRPdFRfmjTa6JiATnnBJZG5CTwd6k",
+  authDomain: "sodium-elf-419523.firebaseapp.com",
+  projectId: "sodium-elf-419523",
+  storageBucket: "sodium-elf-419523.appspot.com",
+  messagingSenderId: "29946781788",
+  appId: "1:29946781788:web:30d6fe382c8538d9ed4699",
+  measurementId: "G-F69SFMENCP"
 };
 
 const app = initializeApp(firebaseConfig);
